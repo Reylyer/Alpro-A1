b8b76509142993744c07e44bb1c83de010e5b990
Reylyer <reylyercross@gmail.com>
2022-03-01 11:34:24 +0700

    push prak 2 - off you go

---
+++ Praktikum2/Bujur_sangkar.c
@@ -0,0 +1,22 @@
+// Nama      : Givandra Haikal Adjie
+// NIM       : 24060121130063 
+// Nama file : Bujur_sangkar.c
+// Deskripsi : menghitung luas bujur sangkar
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    // kamus    
+    int sisi;
+
+    // prompt and input
+    printf("Masukkan sisi: ");
+    scanf("%d", &sisi);
+
+    // output
+    printf("luas: %d", sisi*sisi);
+
+    return 0;
+}
